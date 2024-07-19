a7556a30c22731d8653a1f4f110f72bef7270de9
Okorie Onyedikachi Daniella <cosmasdaniella@gmail.com>
2024-07-19 23:15:41 +0100

    Hero section file

---
+++ src/Components/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/heroSection.css'
+// import heroImage from "../assets/hero-image.jpeg"
+
+const HeroSection = () => {
+  return (
+    <div className='container'>
+        <h1>Pokedex</h1>
+    </div>
+  )
+}
+
+export default HeroSection
