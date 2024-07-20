ecdafc8d8f5bd2c3adf333da149416aff9a6eabf
Okorie Onyedikachi Daniella <cosmasdaniella@gmail.com>
2024-07-20 23:12:02 +0100

    UI: Pagination

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import '../styles/pagination.css'
 const Pagination = () => {
   return (
-    <div>Pagination</div>
+    <div className='pagination-wrapper'>
+        <div className='button'>Prev</div>
+        <div className='button'>Next</div>
+    </div>
   )
 }
 
