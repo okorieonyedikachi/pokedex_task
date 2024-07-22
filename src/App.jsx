2100e76d0dfca5d651ce019e38a976a9a49697db
Okorie Onyedikachi Daniella <cosmasdaniella@gmail.com>
2024-07-22 06:16:27 +0100

    build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './styles/App.css'
-import CardList from './components/CardList'
+import CardList from './components/CardList.jsx'
 import HeroSection from './components/HeroSection'
 
 
